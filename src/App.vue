f6efe0b30795a0fd079de3baee8d5943defaa3b2
laranjazda <carloseng.jr@gmail.com>
2022-04-23 00:41:19 -0300

    photo panel component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto in fotos" :key="foto">
-        <div>
-          <p>{{ foto.titulo }}</p>
-          <img :src="foto.url" :alt="foto.titulo" />
-        </div>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <input 
+    type="search" 
+    class="filtro" 
+    placeholder="Filtre por descriçao da foto"
+    v-on:input="filtro = $event.target.value">
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in photosFilter" :key="foto">
+        <photoPanel :title="foto.description">
+            <img
+              class="imagem-responsiva"
+              :src="foto.url"
+              :alt="foto.description"
+            />
+        </photoPanel>
       </li>
     </ul>
   </div>
@@ -14,11 +24,26 @@
 
 <script>
 import axios from "axios";
+import photoPanel from "./components/photoPanel/photoPanel.vue"
 export default {
+  components:{
+    photoPanel
+  },
+  computed:{
+    photosFilter(){
+      if(this.filtro){
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.description))
+      } else {
+        return this.fotos
+      }
+    }
+  },
   data() {
     return {
       titulo: "H.S.O.B. Service",
       fotos: [],
+      filtro:''
     };
   },
   created() {
@@ -39,5 +64,31 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.centralizado {
+  text-align: center;
+}
+
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 98%;
+}
+
+.filtro {
+    display: block;
+    width: 100%;
+  }
 </style>
